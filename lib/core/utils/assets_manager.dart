3bdad85a62c069982da94e46badb9c34863a1460
Abdullah <abdullhahmed107@gmail.com>
2026-01-29 19:40:21 +0200

    clear unused assets

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -3,14 +3,10 @@ class AssetsManager {
   static const String splashLogo = 'assets/images/splash_logo.png';
   static const String onBoarding = 'assets/images/onboarding.png';
   static const String appLogo = 'assets/images/app_logo.png';
-  static const String logoImage = 'assets/images/logo.png';
   static const String googleIcon = 'assets/images/google.png';
   static const String appleIcon = 'assets/images/Apple.png';
-  static const String mockawel = "assets/images/mockawel.png";
   static const String successCheck = "assets/images/success_check.json";
   static const String wrongAnimation = "assets/images/wrong.json";
-  static const String homeBanner = "assets/images/home_banner.png";
-  static const String contractor = "assets/images/contractor.png";
   static const String subscriptionPackageImageWithoutBackground =
       "assets/images/subscription_package_image.png";
   static const String subscriptionPackageImage =
