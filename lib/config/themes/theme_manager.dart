c0140935e0fcdd8cc35332321d2c5e1cd446bf74
marwaali <Devteam2030@gmail.com>
2026-08-27 08:56:07 +0300

    Prepare iOS release configuration

--- lib/config/themes/theme_manager.dart
+++ lib/config/themes/theme_manager.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mokawlcom_app/core/utils/assets_manager.dart';
